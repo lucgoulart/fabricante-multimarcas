d42e8ca7fae097c4ec880a29c7c772cc9a12328d
LucasGsalman <salmanlucas17@outlook.com>
2024-05-27 18:38:09 -0300

    header - desktop

---
+++ src/app/app-module.ts
@@ -0,0 +1,34 @@
+import { PaginaInicialComponent } from './components/pagina-inicial/pagina-inicial.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PaginaInicialComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+
+  ],
+  exports: [
+    HeaderComponent,  // Exporte o HeaderComponent se necessário
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,15 @@
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PaginaInicialComponent } from './components/pagina-inicial/pagina-inicial.component';
+
+const routes: Routes = [
+   { path: '', component: PaginaInicialComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
